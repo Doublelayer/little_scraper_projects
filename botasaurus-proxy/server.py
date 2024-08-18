123557a1584c4f188376d4d9aa732e0ee8d0a351
F.H <hamann.florin.dev@gmail.com>
2024-08-18 15:09:07 +0200

    adjust Dockerfile_Botasaurus_Proxy

--- botasaurus-proxy/server.py
+++ botasaurus-proxy/server.py
@@ -7,10 +7,14 @@ from flask import Flask, request
 
 @browser(
     headless=True,
-    block_images=False,
-    reuse_driver=True
+    block_images_and_css=True,
+    wait_for_complete_page_load=False,
+    close_on_crash=True,
+    reuse_driver=False,
+    raise_exception=False
+
 )
-def scrape_html(driver: Driver, url):
+def get_immoscout_cookies(driver: Driver, url):
     driver.google_get(url)
 
     time.sleep(1)
@@ -22,15 +26,16 @@ def scrape_html(driver: Driver, url):
         if time.time() - now > Wait.VERY_LONG:
             raise ElementWithSelectorNotFoundException("cookie 'eveD' not found")
 
-    return driver.page_html
+    return driver.get_cookies_dict()
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/immoscout")
 def get():
-    return scrape_html(request.args.get('url'))
+    return get_immoscout_cookies(
+        "https://www.immobilienscout24.de/Suche/de/berlin/berlin/wohnung-mieten?enteredFrom=one_step_search/")
 
 
-app.run(debug=False, host="0.0.0.0", port=5000)
+app.run(debug=False, host="0.0.0.0", port=5111)
